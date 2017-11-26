dd29d5abf5b4d4eed66d603a50b31df04c1f6cad
Ian Tabolt <ian@tabo.lt>
2017-11-25 23:22:24 -0500

    Add an init shell script

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install fzf
+brew install tree
+brew install emacs --with-cocoa
+brew install fish
+
