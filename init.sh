0f9eb8109683e11f4411bc37ef2c364147cfd950
Ian Tabolt <iantabolt@foursquare.com>
2018-05-21 13:37:55 -0400

    Edit fzf install

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install fzf
+yes | /usr/local/opt/fzf/install
 brew install tree
 brew install emacs --with-cocoa
 brew install fish
