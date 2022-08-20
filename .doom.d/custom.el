f3011876e48ac2958ffd57cbd9e59c8d045a6901
Ian Tabolt <ian@metropolis.io>
2022-08-19 20:03:06 -0400

    Update doom things

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,12 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(doom-monokai-pro))
  '(custom-safe-themes
-   '("1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" default))
- '(package-selected-packages '(mwim gitconfig typescript-mode fish-mode lsp-metals)))
+   '("1bddd01e6851f5c4336f7d16c56934513d41cc3d0233863760d1798e74809b4b" "23c806e34594a583ea5bbf5adf9a964afe4f28b4467d28777bcba0d35aa0872e" "1d44ec8ec6ec6e6be32f2f73edf398620bb721afeed50f75df6b12ccff0fbb15" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "6c98bc9f39e8f8fd6da5b9c74a624cbb3782b4be8abae8fd84cbc43053d7c175" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "76ed126dd3c3b653601ec8447f28d8e71a59be07d010cd96c55794c3008df4d7" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" default))
+ '(package-selected-packages
+   '(play-routes-mode mwim gitconfig typescript-mode fish-mode lsp-metals)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-comment-face ((t (:foreground "#727072" :slant italic :weight ultra-light))))
+ '(font-lock-doc-face ((t (:inherit font-lock-comment-face :foreground "#FFD866" :slant italic))))
+ '(lsp-face-semhl-method ((t (:inherit lsp-face-semhl-function :family "FiraCode"))))
+ '(scala-font-lock:implicit-face ((t (:inherit font-lock-keyword-face)))))
+(put 'customize-themes 'disabled nil)
+(put 'projectile-ripgrep 'disabled nil)
